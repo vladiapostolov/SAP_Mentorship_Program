db45a88b4d90aea083b27024f45d412a8d524d2d
Vladimir Apostolov <vladimirapostolov@Vladimirs-MacBook-Pro.local>
2025-12-23 14:38:09 +0200

    Modified the DB config

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+    SECRET_KEY = "dev-secret-key"
+
+    DB_HOST = "127.0.0.1"
+    DB_USER = "root"
+    DB_PASSWORD = "vladi2004"
+    DB_NAME = "Dronify"
--- warehouse_app/config.py
+++
@@ -1,7 +0,0 @@
-class Config:
-    SECRET_KEY = "dev-secret-key"
-
-    DB_HOST = "localhost"
-    DB_USER = "warehouse_user"
-    DB_PASSWORD = "password"
-    DB_NAME = "warehouse_db"
